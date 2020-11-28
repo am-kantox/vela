50acc662eb95f0144e7bff68d68d6ab49b34d594
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-11-28 05:49:37 +0100

    Make dialyzer happy

--- .credo.exs
+++ .credo.exs
@@ -15,7 +15,8 @@
         {Credo.Check.Design.AliasUsage, exit_status: 0},
         {Credo.Check.Design.TagTODO, exit_status: 0},
         {Credo.Check.Design.TagFIXME, exit_status: 0},
-        {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 12}
+        {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 12},
+        {Credo.Check.Warning.ApplicationConfigInModuleAttribute, false}
       ]
     }
   ]
