9f04c72ed52b5c4b7ab6c7f0471f828d97196721
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2023-02-26 18:21:14 +0100

    Better docs

--- examples/vela_impl.ex
+++ examples/vela_impl.ex
@@ -2,6 +2,27 @@ defmodule WeatherForecast do
   @moduledoc """
   The example of `Vela` implementation, demontrating the real-life
   module using `Vela` for timeseries control. 
+
+  The full code of this file follows.
+
+  ```elixir
+  defmodule WeatherForecast do
+    alias WeatherForecast, as: Me
+    use Vela,
+      __globals__: [validator: Me],
+      tomorrow: [limit: 5, errors: 1],
+      two_days: [limit: 3],
+      week: [limit: 2, sorter: &Me.sort/2]
+
+    @behaviour Vela.Validator
+
+    @impl Vela.Validator
+    def valid?(_serie, value), do: value > 0
+
+    @doc "Sort function to be used as `:sorter` in series"
+    def sort(v1, v2), do: v1 <= v2
+  end
+  ```
   """
 
   use Boundary, deps: [Vela]
