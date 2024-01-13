e9466643a61a32b3c1acb6e556f98790961ea676
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2024-01-13 05:43:54 +0100

    dialyzer

--- .credo.exs
+++ .credo.exs
@@ -11,7 +11,7 @@
         {Credo.Check.Consistency.TabsOrSpaces},
         {Credo.Check.Readability.MaxLineLength, max_length: 120},
         {Credo.Check.Refactor.LongQuoteBlocks, max_line_count: 250},
-        {Credo.Check.Refactor.Nesting, max_nesting: 3},
+        {Credo.Check.Refactor.Nesting, max_nesting: 4},
         {Credo.Check.Design.AliasUsage, exit_status: 0},
         {Credo.Check.Design.TagTODO, exit_status: 0},
         {Credo.Check.Design.TagFIXME, exit_status: 0},
